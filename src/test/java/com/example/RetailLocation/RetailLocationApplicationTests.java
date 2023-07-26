fa22b0480ca062377296b207d29850e51bc8ef69
Rk212121 <rohitskadam8@gmail.com>
2023-07-26 19:47:47 +0530

    This is 1st project of Retail Loction

---
+++ src/test/java/com/example/RetailLocation/RetailLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RetailLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
