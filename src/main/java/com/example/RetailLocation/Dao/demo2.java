18842b560689737ce84ab5e5369c6ae91c815d8e
Rk212121 <rohitskadam8@gmail.com>
2023-07-28 18:36:45 +0530

    this is stash commit

---
+++ src/main/java/com/example/RetailLocation/Dao/demo2.java
@@ -0,0 +1,10 @@
+package com.example.RetailLocation.Dao;
+
+public class demo2 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Demo2");
+	}
+
+}
---
+++ src/main/java/com/example/RetailLocation/Service/Service.java
@@ -0,0 +1,16 @@
+package com.example.RetailLocation.Service;
+
+public class Service {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is demo service");
+		
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		int d = 40;
+		
+	}
+
+}
